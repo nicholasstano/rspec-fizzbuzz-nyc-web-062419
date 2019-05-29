e8d2b385d16fdb29514593fbc94aed84be68716c
Nicholas Stano <nicholasstano@gmail.com>
2019-05-29 23:33:56 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(n)
   if ((n % 3 == 0) && (n % 5 == 0))
-    return "Fizzbuzz"
+    return "FizzBuzz"
   elsif (n % 3 == 0)
     return "Fizz"
   elsif (n % 5 == 0)
