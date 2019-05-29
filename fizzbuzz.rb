3fd5cf227f7553ce241d7c5605656d4bcd004611
Nicholas Stano <nicholasstano@gmail.com>
2019-05-29 23:27:08 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(n)
+  if ((n % 3) && (n % 5))
+    return "fizzbuzz"
+  elsif (n % 3) 
+    return "fizz"
+  elsif (n % 5) 
+    return "buzz" 
+  else 
+    return "nil"
